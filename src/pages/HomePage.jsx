6ae5f09188407bb18061f977d565202a1ebdcf79
Tahir Rashid <tahirbhai736333@gmail.com>
2026-02-08 19:26:45 +0500

    created components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SideBar from '../components/SideBar';
+import ChatContainer from '../components/ChatContainer';
+import RightSidebar from '../components/RightSidebar';
 
 const HomePage = () => {
+    const [selectedUser, setSelectedUser] = useState(false)
   return (
-    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>HomePage</div>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+        <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]':'md:grid-cols-2'}`}>
+            <SideBar/>
+            <ChatContainer/>
+            <RightSidebar/>
+        </div>
+    </div>
   )
 }
 
