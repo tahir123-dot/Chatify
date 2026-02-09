b5dc969fb2c390c154f9b1c65a9315be9dd69ce0
Tahir Rashid <tahirbhai736333@gmail.com>
2026-02-09 18:58:11 +0500

    start chat container where see messages of users

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import assets from "../assets/assets";
 
-const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
-}
+const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+  return selectedUser ? (
+    <div className="h-full overflow-y-auto no-scrollbar relative backdrop-blur-lg">
+      <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
+        <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
+        <p className="flex flex-1 text-lg text-white items-center gap-2">
+          Matin Johnson
+          <span className="w-2 h-2 rounded-full bg-green-500"></span>
+        </p>
+        <img
+          onClick={() => setSelectedUser(null)}
+          src={assets.arrow_icon}
+          alt=""
+          className="md:hidden max-w-7"
+        />
+        <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden">
+      <img src={assets.logo_icon} className="max-w-16" alt="" />
+      <p className="text-lg font-medium text-white">Chat anytime, anywhere</p>
+    </div>
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
