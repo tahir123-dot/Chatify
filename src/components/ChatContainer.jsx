4f2944d3f5a454ac0b0eb297325eafbe02ca2719
Tahir Rashid <tahirbhai736333@gmail.com>
2026-02-16 21:53:49 +0500

    adding chatbontainer

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import assets from "../assets/assets";
+import assets, { messagesDummyData } from "../assets/assets";
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
   return selectedUser ? (
@@ -17,6 +17,41 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
           className="md:hidden max-w-7"
         />
         <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
+      </div>
+      {/* chat area*/}
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+        {messagesDummyData.map((msd, index) => (
+          <div
+            key={index}
+            className={`flex items-end gap-2 justify-end ${msg.senderId !== "546333553535224" && "flex-row-reverse"}`}
+          >
+            {msg.image ? (
+              <img
+                src={assets.image}
+                alt=""
+                className="max-w-57.5 border border-gray-700 rounded-lg overflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-57.5 md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === "232423422" ? "rounded-br-none" : "rounded-bl-none"}`}
+              >
+                {msg.text}
+              </p>
+            )}
+            <div className="text-center text-xs">
+              <img
+                src={
+                  msg.senderId === "020240242"
+                    ? assets.avatar_icon
+                    : assets.profile_martin
+                }
+                alt=""
+                className="w-7 rounded-full"
+              />
+              <p className="text-gray-500">{msg.createdAt}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   ) : (
