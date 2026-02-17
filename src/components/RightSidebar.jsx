f3d26bfeb424f4c81ad4b7919d1cb4872c726fce
Tahir Rashid <tahirbhai736333@gmail.com>
2026-02-18 00:36:12 +0500

    added RightSide bar of chatUi with logout button

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import assets, { imagesDummyData } from "../assets/assets";
 
-const RightSidebar = () => {
+const RightSidebar = ({ selectedUser }) => {
   return (
-    <div>RightSidebar</div>
-  )
-}
+    selectedUser && (
+      <div
+        className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : ""}`}
+      >
+        <div className="pt pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto">
+          <img
+            src={selectedUser?.profilePic || assets.avatar_icon}
+            alt=""
+            className="w-20 aspect-square rounded-full"
+          />
+          <h1 className="text-xl px-10 font-medium mx-auto flex items-center gap-2">
+            {selectedUser.fullName}
+          </h1>
+          <p className="px-10 mx-auto">{selectedUser.bio}</p>
+        </div>
 
-export default RightSidebar
+        <hr className="border-[#ffff50] my-4" />
+
+        <div className="px-5 text-xs">
+          <p>Media</p>
+          <div className="mt mt-2 max-h-50  grid grid-cols-2 gap-4 opacity-80">
+            {imagesDummyData.map((url, index) => (
+              <div
+                key={index}
+                onClick={() => window.open(url)}
+                className="cursor-pointer rounded"
+              >
+                <img src={url} alt="" className="h-full rounded-md" />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button className="absolute bottom-5 left-1/2 transform translate-x-1/2 bg-linear-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursorpoi">
+          Logout
+        </button>
+      </div>
+    )
+  );
+};
+
+export default RightSidebar;
