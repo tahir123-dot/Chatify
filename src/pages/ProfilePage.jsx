762d8156952a6fca11b86eb16a8e9ad3dade63cf
Tahir Rashid <tahirbhai736333@gmail.com>
2026-02-23 23:50:31 +0500

    building profile page ui design

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
-  )
-}
+    <div className="h min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profile details</h3>
+        </form>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
 
 export default ProfilePage;
